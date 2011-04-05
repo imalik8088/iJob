8328c76ed94d470d0e5b74934a50b04c4ee59bfa
Willy Wonka <akhlaq87@googlemail.com>
2011-04-05 15:28:12 +0200

    Startseite wurde geändert

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 IJob::Application.routes.draw do
+  get "home/index"
+
   resources :unemployeds
 
   resources :bussiness_agents
@@ -68,7 +70,7 @@ IJob::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
